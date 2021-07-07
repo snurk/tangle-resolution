f364baf30d1a6c7820cd1d7cf04ac200944e80d9
Sergey Nurk <sergeynurk@gmail.com>
2021-07-07 00:35:39 -0400

    even K DBG bluntification

--- scripts/bluntify_dbg.py
+++ scripts/bluntify_dbg.py
@@ -1,14 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+from __future__ import division
+from __future__ import print_function
 
 import sys
 
-k = int(sys.argv[1])
+if len(sys.argv) < 2:
+	print("Usage: %s <k>" % sys.argv[0])
+	sys.exit(1)
+
+ovl = int(sys.argv[1])
 
 # graph from stdin
 # graph to stdout
 
-assert k % 2 == 1
-remove_overlap = (k-1) / 2
+#assert ovl % 2 == 0
+remove_overlap = ovl // 2
+
+print("H\tVN:Z:1.0")
 
 node_seqs = {}
 has_edge = set()
@@ -17,7 +25,7 @@ for l in sys.stdin:
 	if l[0] == 'S':
 		node_seqs[parts[1]] = parts[2]
 	if l[0] == 'L':
-		print("L\t" + parts[1] + "\t" + parts[2] + "\t" + parts[3] + "\t" + parts[4] + "\t0M")
+		print("L\t%s\t%s\t%s\t%s\t%dM" % (parts[1], parts[2], parts[3], parts[4], ovl % 2))
 		has_edge.add((parts[1], parts[2] == "+"))
 		has_edge.add((parts[3], parts[4] == "-"))
 
